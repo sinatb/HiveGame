d0b7cfbe482f18a862ce90501ea226162adebae8
Meysam Bavi <meybsky@gmail.com>
2022-04-16 15:23:30 +0430

    Added pieces list to HexPlace

--- src/hex_place.py
+++ src/hex_place.py
@@ -1,8 +1,22 @@
-from constants import *
-
 
 class HexPlace:
-    def __init__(self):
-        self.piece = NO_PIECE
-        self.player_num = -1
+    def __init__(self, pos):
+        self.pos = pos
+        self._pieces = []
 
+    def _get_top_piece(self):
+        return self._pieces[-1]
+
+    def _set_top_piece(self, piece):
+        self._pieces.append(piece)
+
+    def pop_top_piece(self):
+        return self._pieces.pop()
+
+    def isEmpty(self):
+        return len(self._pieces) <= 0
+
+    def isNotEmpty(self):
+        return not self.isEmpty()
+
+    top_piece = property(_get_top_piece, _set_top_piece)
