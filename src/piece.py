fb62434fd2aa49b9e3ed80ed94dad78f4310aa5c
Meysam Bavi <meybsky@gmail.com>
2022-04-16 15:24:05 +0430

    Added piece class and changed player methods

---
+++ src/piece.py
@@ -0,0 +1,5 @@
+class Piece:
+    def __init__(self, piece_type, player):
+        self.type = piece_type
+        self.player = player
+        self.pos = None
--- src/player.py
+++ src/player.py
@@ -1,21 +1,22 @@
 from constants import *
+from src.piece import Piece
 
 
 class Player:
     def __init__(self, num):
-        self.pieces = {QUEEN: 1, ANT: 3, GRASSHOPPER: 3, COCKROACH: 2, SPIDER: 2}
         self.num = num
+        self._pieces = {QUEEN: [Piece(QUEEN, self.num)],
+                        ANT: [Piece(ANT, self.num) for _ in range(3)],
+                        GRASSHOPPER: [Piece(GRASSHOPPER, self.num) for _ in range(3)],
+                        COCKROACH: [Piece(COCKROACH, self.num) for _ in range(2)],
+                        SPIDER: [Piece(SPIDER, self.num) for _ in range(2)]
+                        }
 
-    def place_piece(self, piece_name, board, i, j):
-        if (self.pieces[piece_name] > 0):
+    def has_free_piece(self, piece_name):
+        return self.get_free_piece_count(piece_name) > 0
 
-            self.pieces[piece_name] -= 1
-            board.set_piece(i, j, piece_name)
-            board.places[i][j].player_num = self.num
-            return True
-        else:
-            return False
+    def get_free_piece(self, piece_name):
+        return self._pieces[piece_name].pop()
 
-    def add_piece(self, piece_name):
-        self.pieces[piece_name] += 1
-
+    def get_free_piece_count(self, piece_name):
+        return len(self._pieces[piece_name])
