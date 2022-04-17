1cc52bd221ab8cbc117ee28cc7088a3147380564
Meysam Bavi <meybsky@gmail.com>
2022-04-17 18:53:44 +0430

    Handling out of range indexes

--- src/board.py
+++ src/board.py
@@ -8,12 +8,12 @@ class Board:
         self.places = [[HexPlace((y, x)) for x in range(self.n)] for y in range(self.m)]
 
     def get_neighbors(self, hex_place):
-        return [self.pos_z_of(hex_place),
-                self.pos_y_of(hex_place),
-                self.pos_x_of(hex_place),
-                self.neg_z_of(hex_place),
-                self.neg_y_of(hex_place),
-                self.neg_x_of(hex_place)]
+        return list(filter(None, [self.pos_z_of(hex_place),
+                                  self.pos_y_of(hex_place),
+                                  self.pos_x_of(hex_place),
+                                  self.neg_z_of(hex_place),
+                                  self.neg_y_of(hex_place),
+                                  self.neg_x_of(hex_place)]))
 
     def get_empty_neighbors(self, hex_place):
         filtered = filter(lambda p: p.isEmpty(), self.get_neighbors(hex_place))
@@ -22,29 +22,32 @@ class Board:
     def pos_x_of(self, hex_place):
         i, j = hex_place.pos
         ii, jj = (i, j + 1)
-        return self.places[ii][jj]
+        return self.places[ii][jj] if self._in_range(ii, jj) else None
 
     def neg_x_of(self, hex_place):
         i, j = hex_place.pos
         ii, jj = (i, j - 1)
-        return self.places[ii][jj]
+        return self.places[ii][jj] if self._in_range(ii, jj) else None
 
     def pos_y_of(self, hex_place):
         i, j = hex_place.pos
         ii, jj = (i - 1, j) if i % 2 == 1 else (i - 1, j + 1)
-        return self.places[ii][jj]
+        return self.places[ii][jj] if self._in_range(ii, jj) else None
 
     def neg_y_of(self, hex_place):
         i, j = hex_place.pos
         ii, jj = (i + 1, j - 1) if i % 2 == 1 else (i + 1, j)
-        return self.places[ii][jj]
+        return self.places[ii][jj] if self._in_range(ii, jj) else None
 
     def pos_z_of(self, hex_place):
         i, j = hex_place.pos
         ii, jj = (i - 1, j - 1) if i % 2 == 1 else (i - 1, j)
-        return self.places[ii][jj]
+        return self.places[ii][jj] if self._in_range(ii, jj) else None
 
     def neg_z_of(self, hex_place):
         i, j = hex_place.pos
         ii, jj = (i + 1, j) if i % 2 == 1 else (i + 1, j + 1)
-        return self.places[ii][jj]
+        return self.places[ii][jj] if self._in_range(ii, jj) else None
+
+    def _in_range(self, i, j):
+        return i < self.m and j < self.n
