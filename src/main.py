75adc4aa5654571fcb61c93554d4ce3809c1e039
sinatb <sinatb.dev@gmail.com>
2022-04-18 05:53:16 +0430

    overall ui changes

--- src/main.py
+++ src/main.py
@@ -8,10 +8,10 @@ import movement
 
 pygame.init()
 pygame.display.set_caption("Hive")
-screen = pygame.display.set_mode((1024, 720));
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT));
 
 # board and the players
-mb = Board(10, 10)
+mb = Board(22, 22)
 
 p1 = Player(1)
 p2 = Player(2)
@@ -68,6 +68,7 @@ G_2.set_colorkey(FULL_WHITE)
 
 # loading the font
 font = pygame.font.Font('../assets/8514fix.fon', 12)
+font1 = pygame.font.Font('../assets/8514fix.fon', 8)
 
 
 def main():
@@ -93,34 +94,34 @@ def main():
                 running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 (x, y) = event.pos
-                if (600 < x < 1024) and (0 < y < 360):
+                if (950 < x < SCREEN_WIDTH) and (0 < y < SCREEN_HEIGHT/2):
                     if not player1_turn():
                         debugger_text = "You can't chose from player 1's pieces"
                     else:
-                        i = math.floor((y - 40) / 75)
-                        j = math.floor((x - 600) / 80)
+                        i = math.floor((y - 40) / 47)
+                        j = math.floor((x - 950) / 50)
                         if i == 0:
                             debugger_text = "Player 1 has chosen " + ALL_PIECES[j]
                             selected_char = ALL_PIECES[j]
                         else:
                             debugger_text = "Chose valid Piece"
 
-                elif (600 < x < 1024) and (360 < y < 720):
+                elif (950 < x < SCREEN_WIDTH) and (SCREEN_HEIGHT/2 < y < SCREEN_HEIGHT):
                     if player1_turn():
                         debugger_text = "You can't chose from player 2's pieces"
                     else:
-                        i = math.floor((y - 400) / 75)
-                        j = math.floor((x - 600) / 80)
+                        i = math.floor((y - 440) / 47)
+                        j = math.floor((x - 950) / 50)
                         if i == 0:
                             debugger_text = "Player 2 has chosen " + ALL_PIECES[j]
                             selected_char = ALL_PIECES[j]
                         else:
                             debugger_text = "Choose valid Piece"
 
-                elif (0 < x < 600) and (0 < y < 500):
-                    i = math.floor(y / 46)
-                    j = math.floor((x - 35 + (0 if (i % 2 == 0) else 25)) / 51)
-                    if (0 <= j < 10 and mb.places[i][j].isEmpty() and
+                elif (0 < x < 950) and (0 < y < 750):
+                    i = math.floor(y / 32)
+                    j = math.floor((x - 35 + (0 if (i % 2 == 0) else 20)) / 40)
+                    if (0 <= j < 22 and mb.places[i][j].isEmpty() and
                             (selected_char != NO_PIECE or moved_piece != NO_PIECE)):
 
                         if selected_char == NO_PIECE and moved_piece != NO_PIECE:
@@ -164,9 +165,9 @@ def draw_deck(player, screen, location):
     ctr = 0
     list_pieces_assets = [queenh, anth, cockroachh, grasshopperh, spiderh]
     for p in list_pieces_assets:
-        screen.blit(p, (location[0] + ctr * 80, location[1]))
-        under = font.render(ALL_PIECES[ctr] + " = " + str(player.get_free_piece_count(ALL_PIECES[ctr])), True, (0, 0, 0))
-        screen.blit(under, (location[0] + ctr * 80 + 15, location[1] + 85))
+        screen.blit(p, (location[0] + ctr * 50, location[1]))
+        under = font1.render(ALL_PIECES[ctr] + " =" + str(player.get_free_piece_count(ALL_PIECES[ctr])), True, (0, 0, 0))
+        screen.blit(under, (location[0] + ctr * 50 + 10, location[1] + 57))
         ctr += 1
 
 
@@ -178,7 +179,8 @@ def draw_field(board, debugger_text, screen, turn, valid_moves):
     for i in range(board.m):
         for j in range(board.n):
             hex_pic = hexagon_valid if board.places[i][j] in valid_moves else hexagon
-            screen.blit(hex_pic, (j * 51 + (0 if (i % 2 == 0) else -25) + 35, i * 46))
+
+            screen.blit(hex_pic, (j * 40 + (0 if (i % 2 == 0) else -20) + 35, i * 32))
             if board.places[i][j].isEmpty():
                 continue
 
@@ -187,34 +189,34 @@ def draw_field(board, debugger_text, screen, turn, valid_moves):
             q_pic, a_pic, c_pic, s_pic, g_pic = (Q_1, A_1, C_1, S_1, G_1) if is_for_p1 else (Q_2, A_2, C_2, S_2, G_2)
 
             if board.places[i][j].top_piece.type == QUEEN:
-                screen.blit(q_pic, (j * 51 + (0 if (i % 2 == 0) else -25) + 37, i * 46 + 15))
+                screen.blit(q_pic, (j * 40 + (0 if (i % 2 == 0) else -25) + 36, i * 32 + 10))
             elif board.places[i][j].top_piece.type == ANT:
-                screen.blit(a_pic, (j * 51 + (0 if (i % 2 == 0) else -25) + 44, i * 46 + 10))
+                screen.blit(a_pic, (j * 40 + (0 if (i % 2 == 0) else -25) + 46, i * 32 + 8))
             elif board.places[i][j].top_piece.type == COCKROACH:
-                screen.blit(c_pic, (j * 51 + (0 if (i % 2 == 0) else -25) + 43, i * 46 + 14))
+                screen.blit(c_pic, (j * 40 + (0 if (i % 2 == 0) else -25) + 43, i * 32 + 14))
             elif board.places[i][j].top_piece.type == SPIDER:
-                screen.blit(s_pic, (j * 51 + (0 if (i % 2 == 0) else -25) + 40, i * 46 + 13))
+                screen.blit(s_pic, (j * 40 + (0 if (i % 2 == 0) else -25) + 40, i * 32 + 13))
             elif board.places[i][j].top_piece.type == GRASSHOPPER:
-                screen.blit(g_pic, (j * 51 + (0 if (i % 2 == 0) else -25) + 38, i * 46 + 12))
+                screen.blit(g_pic, (j * 40 + (0 if (i % 2 == 0) else -25) + 38, i * 32 + 12))
 
     # field lines
-    pygame.draw.line(screen, BLACK, (0, 500), (600, 500))
-    pygame.draw.line(screen, BLACK, (600, 0), (600, 720))
-    pygame.draw.line(screen, BLACK, (600, 360), (1024, 360))
-
-    # texts
+    pygame.draw.line(screen, BLACK, (0, 750), (950, 750))
+    pygame.draw.line(screen, BLACK, (950, 0), (950, SCREEN_HEIGHT))
+    pygame.draw.line(screen, BLACK, (950, SCREEN_HEIGHT/2), (SCREEN_WIDTH, SCREEN_HEIGHT/2))
+    #
+    # # texts
     debugger_color = RED if turn % 2 == 0 else BLUE
-
+    #
     debugger = font.render(debugger_text, True, debugger_color)
-    screen.blit(debugger, (10, 510))
+    screen.blit(debugger, (10, 760))
     player1text = font.render("Player 1", True, BLUE)
-    screen.blit(player1text, (782, 10))
+    screen.blit(player1text, (1082, 10))
     player2text = font.render("Player 2", True, RED)
-    screen.blit(player2text, (782, 370))
-
-    # player deck drawer
-    draw_deck(p1, screen, (600, 40))
-    draw_deck(p2, screen, (600, 400))
+    screen.blit(player2text, (1082, SCREEN_HEIGHT/2 + 10))
+    #
+    # # player deck drawer
+    draw_deck(p1, screen, (950, 40))
+    draw_deck(p2, screen, (950, SCREEN_HEIGHT/2 + 40))
 
     # show
     pygame.display.flip()
